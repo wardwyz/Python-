f960b48db176c3f98189f52218ea3d7440fc72cd
wardwyz <287016183@qq.com>
2020-05-18 18:11:50 +0800

    20200518

---
+++ test.py
@@ -0,0 +1,12 @@
+source = {'a':{'b':1,'c':2},'d':{'e':3,'f':{'g':4}}}
+target = {}
+def flatmap(src,prefix=''):
+    for k,v in src.items():
+        if isinstance(v,(list,tuple,set,dict)):
+            flatmap(v,prefix=prefix+k+'.')
+        else:
+            target[prefix+k] = v
+
+flatmap(source)
+print(target)
+            
